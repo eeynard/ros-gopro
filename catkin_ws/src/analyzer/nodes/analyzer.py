5059b4f9c93a0bba0dbf159ca5223d5b824fe7a5
Tanguy Falconnet <tanguy.falco@gmail.com>
2016-01-06 09:50:28 +0100

    Utilisation du flux vidéo de la GoPro pour analyse et recherche de visage

--- catkin_ws/src/analyzer/nodes/analyzer.py
+++ catkin_ws/src/analyzer/nodes/analyzer.py
@@ -3,6 +3,7 @@ import numpy as np
 import cv2
 import rospy
 import math
+import rospkg
 from sensor_msgs.msg import Image
 from std_msgs.msg import Int64
 from gopro.msg import Status
@@ -12,31 +13,33 @@ from rect.rect import Rect
 class Analyzer:
     
 	def callbackGoProPicture(self, data):
-        #cv2.destroyAllWindows()
 		try:
-		    cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-		    analyze(cv_image)
+		    cv_image = self.bridge.imgmsg_to_cv2(data)
+		    self.analyze(cv_image)
 		except CvBridgeError as e:
-			rospy.logerr("BAD IMAGE")
+			rospy.logerr("PB Bridge : ")
+			rospy.logerr(e)
 		
     
 	def callbackGoProStatus(self, data):
 	    cameraSX = data.sx
 	    if cameraSX :
-	        if cameraSX.videres in self.videoResolutions:
-				self.vidRes = self.videoResolutions[cameraSX.videres]
-	        self.fov = cameraSX.fov
+	        if cameraSX.vidres in self.videoResolutions:
+				self.vidRes = self.videoResolutions[cameraSX.vidres]
+	        self.fov = float(cameraSX.fov)
 	
 	def __init__(self):
-		self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-		self.eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+		rospack = rospkg.RosPack()
+		path = rospack.get_path('analyzer')
+		self.face_cascade = cv2.CascadeClassifier(path + '/resources/haarcascade_frontalface_default.xml')
+		self.eye_cascade = cv2.CascadeClassifier(path + '/resources/haarcascade_eye.xml')
 		self.bridge = CvBridge()
 		
 		#self.cameraHorizontalAngles = {'wide' : 170, 'normal' : 127, 'narrow' : 90}
 		self.videoResolutions = {'720p SuperView' : (9.0/16.0), '920p' : (3.0/4.0)}
 		
 		self.vidRes = self.videoResolutions['720p SuperView']
-		self.fov = 170
+		self.fov = 170.0
         
         rospy.init_node('analyzer', anonymous=True)
         
@@ -54,8 +57,8 @@ class Analyzer:
 	
 	def analyze(self, img):
 		rospy.logerr("Analyzing...")
-		
 		# Determine vertical video angle
+		
 		verticalAngle = self.fov * self.vidRes
 		
 		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -67,15 +70,14 @@ class Analyzer:
 			rectangle = Rect((x,y,w,h))
 			(xCent, yCent) = rectangle.get_center()
 			
-			cv2.circle(img, (int(xCent), int(yCent)), 3, (0, 255, 0), -1)
-			cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-			
 			roi_gray = gray[y:y+h, x:x+w]
 			roi_color = img[y:y+h, x:x+w]
 			eyes = self.eye_cascade.detectMultiScale(roi_gray)
 			
 			if len(eyes) >= 2 :
-				cv2.putText(img,"Face detected", (0,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255))
+				cv2.putText(img,"Face detected", (0,150), cv2.FONT_HERSHEY_SIMPLEX, 5, (0,0,255), 5)
+				cv2.circle(img, (int(xCent), int(yCent)), 3, (0, 255, 0), -1)
+				cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 				
 				#Compute distance between screen center and face center in degree
 				horizontalDistanceAngle = 0
@@ -83,23 +85,21 @@ class Analyzer:
 				if xCent < (width/2.0):
 					distance = math.fabs(xCent - (width/2.0))
 					horizontalDistanceAngle = self.fov * distance / width
-					print "Left : " + str(horizontalDistanceAngle) + "degrees"
+					rospy.logerr("Left : " + str(horizontalDistanceAngle) + " degrees")
 				elif xCent > (width/2.0):
 					distance = math.fabs(xCent - (width/2.0))
 					horizontalDistanceAngle = self.fov * distance / width
-					print "Right : " + str(horizontalDistanceAngle) + "degrees"
+					rospy.logerr("Right : " + str(horizontalDistanceAngle) + " degrees")
 				if yCent < (height/2.0):
 					distance = math.fabs(yCent - (height/2.0))
 					verticalDistanceAngle = verticalAngle * distance / height
-					print "Top : " + str(verticalDistanceAngle) + "degrees"
+					rospy.logerr("Top : " + str(verticalDistanceAngle) + " degrees")
 				elif yCent > (height/2.0):
 					distance = math.fabs(yCent - (height/2.0))
 					verticalDistanceAngle = verticalAngle * distance / height
-					print "Bottom : " + str(verticalDistanceAngle) + "degrees"
+					rospy.logerr("Bottom : " + str(verticalDistanceAngle) + " degrees")
 
-		cv2.imshow('img',img)
-		cv2.waitKey(0)
-		cv2.destroyAllWindows()
+		cv2.imwrite('img.jpg', img)
 
 if __name__ == '__main__':
     anal = Analyzer()
