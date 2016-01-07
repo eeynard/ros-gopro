66fffb3b94a229c8f4d3dc66c2c4fbaa57847002
eeynard <edouard.eynard@gmail.com>
2016-01-07 15:48:41 +0100

    Integrate ROS with arduino

--- arduino/gimbal-controller.ino
+++ arduino/gimbal-controller.ino
@@ -1,4 +1,13 @@
-#include <Servo.h>
+
+#if (ARDUINO >= 100)
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+
+#include <Servo.h> 
+#include <ros.h>
+#include <std_msgs/UInt16.h>
 
 // Pins bind
 #define RGP_PIN_PITCH_CONTROL 2
@@ -12,51 +21,67 @@
 #define RGP_MIN_HEADING_ANGLE 90
 #define RGP_MAX_HEADING_ANGLE 90
 
+#define RGP_DEFAULT_MODE 3
 #define RGP_MIN_PULSE_WIDTH 1000
 #define RGP_MAX_PULSE_WIDTH 2000
 #define RGP_MOVING_DELAY 100
+
+// Ros topics names
+#define RGP_TOPIC_PITCH "RGP_pitch_control"
+#define RGP_TOPIC_HEADING "RGP_heading_control"
+#define RGP_TOPIC_MODE "RGP_mode_switch"
+
+ros::NodeHandle  nh;
 
 Servo pitch; 
 Servo heading;
 Servo mode;
 
-int pos = 0;
+int isAuthorized(int servo, int angle) {
+  return (servo == RGP_PIN_PITCH_CONTROL && (RGP_MIN_PITCH_ANGLE <= angle && angle <= RGP_MAX_PITCH_ANGLE)) ||
+    (servo == RGP_PIN_HEADING_CONTROL && (RGP_MIN_HEADING_ANGLE <= angle && angle <= RGP_MAX_HEADING_ANGLE));
+}
 
-void setup() {
+void pitch_handler( const std_msgs::UInt16& cmd_msg){
+  if (isAuthorized(RGP_PIN_PITCH_CONTROL, cmd_msg.data)) {
+    pitch.write(cmd_msg.data);
+  }
+}
+
+void heading_handler( const std_msgs::UInt16& cmd_msg){
+  if (isAuthorized(RGP_PIN_HEADING_CONTROL, cmd_msg.data)) {
+    heading.write(cmd_msg.data);
+  }
+}
+
+void mode_handler( const std_msgs::UInt16& cmd_msg){
+  if (1 <= cmd_msg.data && cmd_msg.data <= 3) {
+    mode.write(cmd_msg.data);
+  }
+}
+
+
+ros::Subscriber<std_msgs::UInt16> sub_pitch(RGP_TOPIC_PITCH, pitch_handler);
+ros::Subscriber<std_msgs::UInt16> sub_heading(RGP_TOPIC_HEADING, heading_handler);
+ros::Subscriber<std_msgs::UInt16> sub_mode(RGP_TOPIC_MODE, mode_handler);
+
+void setup(){
   mode.attach(RGP_PIN_MODE_SWITCH, RGP_MIN_PULSE_WIDTH, RGP_MAX_PULSE_WIDTH);
-  mode.write(3);
+  mode.write(RGP_DEFAULT_MODE);
   
   pitch.attach(RGP_PIN_PITCH_CONTROL, RGP_MIN_PULSE_WIDTH, RGP_MAX_PULSE_WIDTH);
   
   heading.attach(RGP_PIN_HEADING_CONTROL, RGP_MIN_PULSE_WIDTH, RGP_MAX_PULSE_WIDTH);  
+
+  pinMode(13, OUTPUT);
+
+  nh.initNode();
+  nh.subscribe(sub_pitch);
+  nh.subscribe(sub_heading);
+  nh.subscribe(sub_mode);
 }
 
-int isAuthorized(int servo, int angle) {
-  return (servo == RGP_PIN_PITCH_CONTROL && (RGP_MIN_PITCH_ANGLE <= angle && angle <= RGP_MAX_PITCH_ANGLE)) ||
-         (servo == RGP_PIN_HEADING_CONTROL && (RGP_MIN_HEADING_ANGLE <= angle && angle <= RGP_MAX_HEADING_ANGLE));
+void loop(){
+  nh.spinOnce();
+  delay(1);
 }
-
-void loop() {
-  for (pos = 0; pos <= 180; pos += 1) { 
-    if (isAuthorized(RGP_PIN_PITCH_CONTROL, pos)) {
-      pitch.write(pos);
-      delay(RGP_MOVING_DELAY);
-    }
-    if (isAuthorized(RGP_PIN_HEADING_CONTROL, pos)) {
-      heading.write(pos);
-      delay(RGP_MOVING_DELAY);
-    }
-  }
-  
-  for (pos = 180; pos >= 0; pos -= 1) { 
-    if (isAuthorized(RGP_PIN_PITCH_CONTROL, pos)) {
-      pitch.write(pos);
-      delay(RGP_MOVING_DELAY);
-    }
-    if (isAuthorized(RGP_PIN_HEADING_CONTROL, pos)) {
-      heading.write(pos);
-      delay(RGP_MOVING_DELAY);
-    }
-  }
-}
-
