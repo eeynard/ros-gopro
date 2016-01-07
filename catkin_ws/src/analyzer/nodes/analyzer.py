c166a4f662e59d3491db16247a42aa44048b9dd8
Tanguy Falconnet <tanguy.falco@gmail.com>
2016-01-07 12:47:16 +0100

    Réorganisation des classes. Ajout du message pour la position des visages. Non fonctionnel

--- catkin_ws/src/analyzer/nodes/analyzer.py
+++ catkin_ws/src/analyzer/nodes/analyzer.py
@@ -1,60 +1,134 @@
-#!/usr/bin/env python
-import cv2
+import threading
 import rospy
+import cv2
+import time
+import math
+import numpy as np
+import rospkg
+from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
-from sensor_msgs.msg import CameraInfo
-from std_msgs.msg import Int64
-from gopro.msg import Status
-from faces_detector import FacesDetector
-
+from std_msgs.msg import Float64
+from analyzer.msg import FacePosition
+from rect.rect import Rect
+  
 class Analyzer:
-    'Callback for /gopro/camera/picture topic'
-    def callbackGoProPicture(self, data):
-        # Get image from GoPro and give it to FacesDetector
-        self.facesDetector.image = data
-        # Publish CameraInfo for RVIZ Camera (do not work)
-        self.cameraInfoPub.publish(CameraInfo())
-        # Ask new picture
-        self.takePicturePub.publish(1)
-
-    'Callback for /gopro/status topic'
-    def callbackGoProStatus(self, data):
-        cameraSX = data.sx
-        if cameraSX :
-            # Get Video Resolution
-            if cameraSX.vidres in self.videoResolutions:
-                self.facesDetector.vidRes = self.videoResolutions[cameraSX.vidres]
-            # Get Horizontal Camera Angle
-            self.facesDetector.fov = float(cameraSX.fov)
-            self.statusSub.unregister()
-            
-    'Init'
+    'Initialisation'
     def __init__(self):
-        # GoPro video resolutions 
-        self.videoResolutions = {'720p SuperView' : (9.0/16.0), '920p' : (3.0/4.0), 'WVGA' : (9.0/16.0)}
-        rospy.init_node('analyzer', anonymous=True)
-        self.initPublish()
-        self.facesDetector = FacesDetector(self.analyzePub, 170.0, self.videoResolutions['720p SuperView'])
-        self.initSubscribe()
-        self.takePicturePub.publish(1)
+        # Image resolutions
+        self.vidRes = None
+        self.verticalAngle = None
+        self.horizontalAngle = None
 
-    'Initialisation of topic subscriptions'
+        # Init node
+        rospy.init_node('Analyzer', anonymous=True)
+        
+        # Find needed resources
+        rospack = rospkg.RosPack()
+        path = rospack.get_path('analyzer')
+        self.face_cascade = cv2.CascadeClassifier(path + '/resources/haarcascade_frontalface_default.xml')
+        self.eye_cascade = cv2.CascadeClassifier(path + '/resources/haarcascade_eye.xml')
+
+        # Bridge for ROS Image to OpenCV Image
+        self.bridge = CvBridge()
+
+        self.initSubscribe()
+        self.initPublish()
+
     def initSubscribe(self):
-        rospy.Subscriber('/gopro/camera/picture', Image, self.callbackGoProPicture)
-        self.statusSub = rospy.Subscriber('/gopro/status', Status, self.callbackGoProStatus)
+        rospy.Subscriber('/analyzer/picture/h_angle', Image, self.callbackPictureHAngle)
+        rospy.Subscriber('/analyzer/picture/vidRes', Image, self.callbackPictureVidRes)
+        rospy.Subscriber('/analyzer/picture/raw', Image, self.callbackPictureRaw)
 
     'Initialisation of topic publications'
     def initPublish(self):
-        self.takePicturePub = rospy.Publisher('/gopro/camera/take_picture', Int64, queue_size=10)
-        self.analyzePub = rospy.Publisher('/analyzer/analyze', Image, queue_size=2)
-        self.cameraInfoPub = rospy.Publisher('/analyzer/camera_info', CameraInfo, queue_size=10)
+        self.verticalAnglePub = rospy.Publisher('/analyzer/picture/v_angle', Float64, queue_size=10)
+        self.facePositionPub = rospy.Publisher('/analyzer/face/position', FacePosition, queue_size=10)
+        self.analyzedPicturePub = rospy.Publisher('/analyzer/picture/analyzed', Image, queue_size=2)
+        self.faceDistancePub = rospy.Publisher('/analyzer/face/distance', Float64, queue_size=2)
+    
+    'Detect faces'
+    def detectFaces(self, img, verticalAngle, horizontalAngle, vidRes):        
+        # Define a divider to resize picture
+        div = float(img.shape[1])/1000.0
 
-    'Launch analysis'
+        # Resize picture
+        small = cv2.resize(img, (0,0), fx=(1/div), fy=(1/div))
+        gray = cv2.cvtColor(small, cv2.COLOR_BGR2GRAY)
+        (height, width, _) = small.shape
+
+        # Draw center point on unresized picture
+        cv2.circle(img, (int(width/2*div), int(height/2*div)), 3, (0, 255, 0), -1)
+
+        # Detect faces in picture
+        faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        # Analyze each face
+        for (x,y,w,h) in faces:
+            cv2.putText(img,"Face detected", (0,150), cv2.FONT_HERSHEY_SIMPLEX, 5, (0,0,255), 5)
+            rectangle = Rect((x,y,w,h))
+
+            # Get face center
+            (xCent, yCent) = rectangle.get_center()
+
+            # Draw face center and face rectangle
+            cv2.circle(img, (int(xCent*div), int(yCent*div)), 3, (0, 255, 0), -1)
+            cv2.rectangle(img,(int(x*div),int(y*div)),(int((x+w)*div),int((y+h)*div)),(255,0,0),5)
+
+            # Try to see if there are eyes
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = small[y:y+h, x:x+w]
+            eyes = self.eye_cascade.detectMultiScale(roi_gray)
+            
+            if len(eyes) >= 2 :
+                cv2.putText(img,"Eyes detected", (0,300), cv2.FONT_HERSHEY_SIMPLEX, 5, (0,0,255), 5)
+            
+            #Compute distance between screen center and face center in degree
+            horizontalDistanceAngle = 0
+            verticalDistanceAngle = 0
+            if xCent < (width/2.0):
+                distance = math.fabs(xCent - (width/2.0))
+                horizontalDistanceAngle = fov * distance / width
+                rospy.logerr("Left : " + str(horizontalDistanceAngle) + " degrees")
+            elif xCent > (width/2.0):
+                distance = math.fabs(xCent - (width/2.0))
+                horizontalDistanceAngle = fov * distance / width
+                rospy.logerr("Right : " + str(horizontalDistanceAngle) + " degrees")
+            if yCent < (height/2.0):
+                distance = math.fabs(yCent - (height/2.0))
+                verticalDistanceAngle = verticalAngle * distance / height
+                rospy.logerr("Top : " + str(verticalDistanceAngle) + " degrees")
+            elif yCent > (height/2.0):
+                distance = math.fabs(yCent - (height/2.0))
+                verticalDistanceAngle = verticalAngle * distance / height
+                rospy.logerr("Bottom : " + str(verticalDistanceAngle) + " degrees")
+                
+        # Publish analyzed picture        
+        self.analyzedPicturePub.publish(self.bridge.cv2_to_imgmsg(img, encoding="passthrough"))
+
+    def callbackPictureHAngle(self, data):
+        self.horizontalAngle = data
+
+    def callbackPictureVidRes(self, data):
+        self.vidRes = data
+
+    def callbackPictureRaw(self, data):
+        if self.horizontalAngle is None or self.vidRes is None:
+            return
+        self.comptureVerticalAngle(self.horizontalAngle, self.vidRes)
+        try:
+            # Convert image from ROS to OpenCV
+            image = self.bridge.imgmsg_to_cv2(data)
+            self.detectFaces(image, self.verticalAngle, self.horizontalAngle, self.vidRes)
+        except CvBridgeError as e:
+            rospy.logerr(e)
+
+    'Determine vertical video angle'
+    def comptureVerticalAngle(self, horizontalAngle, vidRes):
+        self.verticalAngle = horizontalAngle * vidRes
+
     def start(self):
-        self.facesDetector.start()
         rospy.spin()
 
 if __name__ == '__main__':
     anal = Analyzer()
     anal.start()
-
