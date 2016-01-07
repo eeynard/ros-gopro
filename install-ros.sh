8ae66abae3a4cceeb2a496767e0808242f5265ab
Vincent Valot <valot.vince@gmail.com>
2016-01-07 14:39:50 +0100

    Added export locale

--- install-ros.sh
+++ install-ros.sh
@@ -25,10 +25,10 @@ python get-pip.py
 # Project dependencies
 apt-get install -y gfortran libxslt1-dev libxml2-dev libxslt-dev python-devRemoved
 
-pip install requests lxml
+pip install requests lxml rect
 
 # Add default rules for 1394
 echo "KERNEL==\"raw1394\", GROUP=\"video\"" > /etc/udev/rules.d/raw1394.rules
 
-# Needed for Analyzer.py script
-pip install rect
+# Added default locale
+export LC_ALL="en_US.UTF-8"
